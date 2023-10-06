5c13b95c88b9ed7ab71bf90d7edd2d7779701067
CONURE-SAQEEF <muhammad.shabbir@conurets.com>
2023-10-06 19:03:41 +0500

    improved movement of svgs, added merge and download buttons

--- src/app/components/svg-maker-dashboard/svg-maker-dashboard.component.ts
+++ src/app/components/svg-maker-dashboard/svg-maker-dashboard.component.ts
@@ -11,6 +11,8 @@ export class SvgMakerDashboardComponent {
   public svgCount: number = 1;
   public draggingElement: any;
 
+  public svgDownloadCount: number = 1;
+
   async onFileSelected(e: Event) {
     this.selectedFile = (e.target as HTMLInputElement).files![0];
 
@@ -160,6 +162,7 @@ export class SvgMakerDashboardComponent {
       newDraggableComponent.addEventListener("mousedown", this.onMouseDown);
       newDraggableComponent.addEventListener("mouseup", this.onMouseUp);
       newDraggableComponent.addEventListener("mouseleave", this.onMouseLeave);
+      newDraggableComponent.addEventListener("click", this.onMouseClick);
 
       svgConatinerDiv?.appendChild(newDraggableComponent);
 
@@ -191,6 +194,76 @@ export class SvgMakerDashboardComponent {
     element.style.left = coord.x + "px";
   }
 
+  onMergeLinkClick() {
+    
+  }
+
+  onDownloadLinkClick() {
+    //get svg element.
+    var svg = document.getElementById("svgPlanComponentDiv")!.getElementsByTagName('svg')[0];
+    var name = 'svg_' + this.svgDownloadCount;
+    saveSvg(svg, name);
+    this.svgDownloadCount++;
+  }
+
+  onMouseClick(event: any): void {
+    if (event.ctrlKey) {
+      let divElement = event.currentTarget as HTMLDivElement;
+
+      let divHeight: number = divElement.getAttribute("height") ? parseInt(divElement.getAttribute("height")!.toString()) : 0;
+      let divWidth: number = divElement.getAttribute("width") ? parseInt(divElement.getAttribute("width")!.toString()) : 0;
+
+      divHeight += 5;
+      divWidth += 5;
+
+      divElement.setAttribute("height", divHeight.toString());
+      divElement.setAttribute("width", divWidth.toString());
+
+      console.log(divElement)
+
+      let svgElement = event.currentTarget.getElementsByTagName('svg')[0] as SVGElement;
+
+      let svgHeight: number = svgElement.getAttribute("height") ? parseInt(svgElement.getAttribute("height")!.toString()) : 0;
+      let svgWidth: number = svgElement.getAttribute("width") ? parseInt(svgElement.getAttribute("width")!.toString()) : 0;
+
+      svgHeight += 5;
+      svgWidth += 5;
+
+      svgElement.setAttribute("height", svgHeight.toString());
+      svgElement.setAttribute("width", svgWidth.toString());
+
+      console.log(svgElement)
+    }
+
+    if (event.altKey) {
+      let divElement = event.currentTarget as HTMLDivElement;
+
+      let divHeight: number = divElement.getAttribute("height") ? parseInt(divElement.getAttribute("height")!.toString()) : 0;
+      let divWidth: number = divElement.getAttribute("width") ? parseInt(divElement.getAttribute("width")!.toString()) : 0;
+
+      divHeight -= 5;
+      divWidth -= 5;
+
+      divElement.setAttribute("height", divHeight.toString());
+      divElement.setAttribute("width", divWidth.toString());
+
+      console.log(divElement)
+
+      let svgElement = event.currentTarget.getElementsByTagName('svg')[0] as SVGElement;
+
+      let svgHeight: number = svgElement.getAttribute("height") ? parseInt(svgElement.getAttribute("height")!.toString()) : 0;
+      let svgWidth: number = svgElement.getAttribute("width") ? parseInt(svgElement.getAttribute("width")!.toString()) : 0;
+
+      svgHeight -= 5;
+      svgWidth -= 5;
+
+      svgElement.setAttribute("height", svgHeight.toString());
+      svgElement.setAttribute("width", svgWidth.toString());
+
+      console.log(svgElement)
+    }
+  }
+
   onMouseMove(event: any): void {
     if (this.draggingElement) {
       // start of positioning code
@@ -199,33 +272,35 @@ export class SvgMakerDashboardComponent {
       let id = data[1];
       let number = data[2];
 
-      if(holder == 'svgComponent')
-      {
+      if (holder == 'svgComponent') {
         // const divPoint = this.getDivPointByItems(event, this.draggingElement);
         // this.setPosition(this.draggingElement, { x: divPoint.x, y: divPoint.y  })
 
-        const conatiner = document.getElementById('items-container');
-        const itemsRect = conatiner!.getBoundingClientRect();
+        // const conatiner = document.getElementById('items-container');
+        // const itemsRect = conatiner!.getBoundingClientRect();
 
-        const rect: any = {};
-        rect.x = itemsRect.x + event.offsetX;
-        rect.y = itemsRect.y + event.offsetY;
+        // const rect: any = {};
+        // rect.x = itemsRect.x + event.offsetX;
+        // rect.y = itemsRect.y + event.offsetY;
 
-        this.draggingElement.style.position = "absolute";
-        this.draggingElement.style.top = rect.y + "px";
-        this.draggingElement.style.left = rect.x + "px";
+        // console.log(itemsRect)
+
+        // this.draggingElement.style.position = "absolute";
+        // this.draggingElement.style.top = rect.y + "px";
+        // this.draggingElement.style.left = rect.x + "px";
       }
-      else if (holder == 'svgContainer')
-      {
+      else if (holder == 'svgContainer') {
         // const divPoint = this.getDivPointBySvg(event, this.draggingElement);
         // this.setPosition(this.draggingElement, { x: divPoint.x, y: divPoint.y  })
 
-        const conatiner = document.getElementById('svg-container');
+        const conatiner = document.getElementById(holder + '_' + id);
         const svgRect = conatiner!.getBoundingClientRect();
 
         const rect: any = {};
         rect.x = svgRect.x + event.offsetX;
         rect.y = svgRect.y + event.offsetY;
+
+        console.log(svgRect)
 
         this.draggingElement.style.position = "absolute";
         this.draggingElement.style.top = rect.y + "px";
@@ -294,6 +369,18 @@ export class SvgMakerDashboardComponent {
 
     return svgPoint;
   }
+}
+
+function saveSvg(svgElement: any, svgName: any) {
+  svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  var svgData = svgElement.outerHTML;
+  var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+  var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
+  var svgUrl = URL.createObjectURL(svgBlob);
+  let anchorElement = document.getElementById("svg-container-download-link")! as HTMLAnchorElement;
+  anchorElement.href = svgUrl;
+  anchorElement.download = svgName;
+  anchorElement.click();
 }
 
 async function getSvgText(file: File) : Promise<any> {
